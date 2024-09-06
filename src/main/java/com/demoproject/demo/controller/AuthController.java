ef39699a6f7141835685db8bd25204df15bbeb44
Keyur Dobariya <keyur.teqheal@gmail.com>
2024-09-06 15:54:45 +0530

    Update AuthController.java

--- src/main/java/com/demoproject/demo/controller/AuthController.java
+++ src/main/java/com/demoproject/demo/controller/AuthController.java
@@ -3,7 +3,6 @@ package com.demoproject.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
